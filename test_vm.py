93e4199d763cdeb741eb348a552fef121b90256b
Dalbongjooin <ldh6346@gmail.com>
2018-08-14 15:39:24 +0900

    ~~

--- test_vm.py
+++ test_vm.py
@@ -1,2 +1,21 @@
-def test_plus():
-    assert 3 == 1 + 2
+from vm import run, init
+#모든 테스트 에서 초기화를 시키는 함수 init
+
+
+def test_initial_change_should_be_zero():
+    init()
+    assert "잔액은 0원입니다" == run("잔액")
+
+def test_insert_coin_and_check():
+    init()
+    assert "100원을 넣었습니다" == run("동전 100")
+    assert "잔액은 100원입니다" == run("잔액")
+
+def test_accumulation_of_change():
+    init()
+    run("동전 100")
+    run("동전 100")
+    assert "잔액은 200원입니다" == run("잔액")
+
+def test_unknown():
+    assert "알 수 없는 명령입니다" == run("dfdf")
