1eddbfc21f5f75bca1dc07f055b54fcdcae6e667
Dalbongjooin <ldh6346@gmail.com>
2018-08-14 16:51:37 +0900

    자판기

--- test_vm.py
+++ test_vm.py
@@ -1,21 +1,22 @@
-from vm import run, init
+from vm import  VendingMachine
 #모든 테스트 에서 초기화를 시키는 함수 init
 
 
 def test_initial_change_should_be_zero():
-    init()
-    assert "잔액은 0원입니다" == run("잔액")
+    m = VendingMachine()
+    assert "잔액은 0원입니다" == m.run("잔액")
 
 def test_insert_coin_and_check():
-    init()
-    assert "100원을 넣었습니다" == run("동전 100")
-    assert "잔액은 100원입니다" == run("잔액")
+    m = VendingMachine()
+    assert "100원을 넣었습니다" == m.run("동전 100")
+    assert "잔액은 100원입니다" == m.run("잔액")
 
 def test_accumulation_of_change():
-    init()
-    run("동전 100")
-    run("동전 100")
-    assert "잔액은 200원입니다" == run("잔액")
+    m = VendingMachine()
+    m.run("동전 100")
+    m.run("동전 100")
+    assert "잔액은 200원입니다" == m.run("잔액")
 
 def test_unknown():
-    assert "알 수 없는 명령입니다" == run("dfdf")
+    m = VendingMachine()
+    assert "알 수 없는 명령입니다" == m.run("cmd")
--- vm.py
+++ vm.py
@@ -1,22 +1,41 @@
 #프로덕션 코드
 change = 0
 
+class VendingMachine:
+    #class의 인스턴스가 무조건 이 과정을 밟게  끔초기화 시켜준다
+    def __init__(self):
+        self.change = 0
 
-def init():
-    global change
-    change = 0
+    def run(self, raw):
+        tokens = raw.split(" ")
+        cmd, params = tokens[0], tokens[1:]
 
-def run(raw):
-    global change
+        if cmd == "잔액" :
+            return "잔액은 " + str(self.change) + "원입니다"
+        elif cmd == "동전":
+            coin = params[0]
+            self.change += int(coin)
+            return coin + "원을 넣었습니다"
+        else:
+            return "알 수 없는 명령입니다"
 
-    tokens = raw.split(" ")
-    cmd, params = tokens[0], tokens[1:]
 
-    if cmd == "잔액" :
-        return "잔액은 " + str(change) + "원입니다"
-    elif cmd == "동전":
-        coin = params[0]
-        change += int(coin)
-        return coin + "원을 넣었습니다"
-    else:
-        return "알 수 없는 명령입니다"
+#
+# def init():
+#     global change
+#     change = 0
+#
+# def run(raw):
+#     global change
+#
+#     tokens = raw.split(" ")
+#     cmd, params = tokens[0], tokens[1:]
+#
+#     if cmd == "잔액" :
+#         return "잔액은 " + str(change) + "원입니다"
+#     elif cmd == "동전":
+#         coin = params[0]
+#         change += int(coin)
+#         return coin + "원을 넣었습니다"
+#     else:
+#         return "알 수 없는 명령입니다"
