89f145658131667e12d8a48a63954dfc15474ebe
Dalbongjooin <ldh6346@gmail.com>
2018-08-14 16:56:29 +0900

    20180814

--- vm.py
+++ vm.py
@@ -4,17 +4,17 @@ change = 0
 class VendingMachine:
     #class의 인스턴스가 무조건 이 과정을 밟게  끔초기화 시켜준다
     def __init__(self):
-        self.change = 0
+        self._change = 0
 
     def run(self, raw):
         tokens = raw.split(" ")
         cmd, params = tokens[0], tokens[1:]
 
         if cmd == "잔액" :
-            return "잔액은 " + str(self.change) + "원입니다"
+            return "잔액은 " + str(self._change) + "원입니다"
         elif cmd == "동전":
             coin = params[0]
-            self.change += int(coin)
+            self._change += int(coin)
             return coin + "원을 넣었습니다"
         else:
             return "알 수 없는 명령입니다"
